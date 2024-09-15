52ac74d979bbc5796d020c1337290a74e9b211c3
insertokername <insertokname@gmail.com>
2024-09-15 10:26:44 +0300

    made project runable via nix run

--- flake.nix
+++ flake.nix
@@ -10,13 +10,16 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+      in {
+        defaultPackage = naersk-lib.buildPackage {
+          name = "cli";
+          src = ./.;
         };
-      }
-    );
+        devShell = with pkgs;
+          mkShell {
+            buildInputs =
+              [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+      });
 }
